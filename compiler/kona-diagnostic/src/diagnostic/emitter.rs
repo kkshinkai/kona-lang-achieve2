ae20e49520a0e867aee95546d5c55aef7c2349a9
Kk Shinkai <kkshinkai@kkshinkai.moe>
2022-09-07 00:56:47 +0800

    Update the TTY emitter

--- compiler/kona-diagnostic/src/diagnostic/emitter.rs
+++ compiler/kona-diagnostic/src/diagnostic/emitter.rs
@@ -1,7 +1,7 @@
 // Copyright (c) Kk Shinkai. All Rights Reserved. See LICENSE.txt in the project
 // root for license information.
 
-use std::rc::Rc;
+use std::{rc::Rc, io};
 
 use crate::source::SourceMap;
 use std::io::Write;
@@ -46,46 +46,51 @@ impl TtyEmitter {
     }
 }
 
-impl Emitter for TtyEmitter {
-    fn emit_diagnostic(&mut self, diag: &Diagnostic) {
-        // Prints colored "error", "warning", or "note".
-        let mut color_spec = ColorSpec::new();
+impl TtyEmitter {
+    fn try_emit_diagnostic(&mut self, diag: &Diagnostic) -> io::Result<()> {
+       // Prints colored "error", "warning", or "note".
+       let mut color_spec = ColorSpec::new();
 
-        color_spec.set_fg(Some(match diag.level {
-            Level::Error => Color::Red,
-            Level::Warn => Color::Yellow,
-            Level::Note => Color::Blue,
-        }));
-        color_spec.set_bold(true);
+       color_spec.set_fg(Some(match diag.level {
+           Level::Error => Color::Red,
+           Level::Warn => Color::Yellow,
+           Level::Note => Color::Blue,
+       }));
+       color_spec.set_bold(true);
 
-        // Prints colored "error", "warning", or "note".
-        self.out.set_color(&color_spec).expect("error: failed to emit error");
-        write!(self.out, "{}", match diag.level {
-            Level::Error => "error",
-            Level::Warn => "warning",
-            Level::Note => "note",
-        }).unwrap();
+       self.out.set_color(&color_spec)?;
+       write!(self.out, "{}", match diag.level {
+           Level::Error => "error",
+           Level::Warn => "warning",
+           Level::Note => "note",
+       })?;
 
-        color_spec.set_fg(None);
-
-        // Prints the message.
-        self.out.set_color(&color_spec).expect("error: failed to emit error");
-        writeln!(self.out, ": {}", diag.message).unwrap();
-
-        // Resets the color and boldness.
-        self.out.reset().expect("error: failed to emit error");
+       color_spec.set_fg(None);
+       color_spec.set_bold(false);
+       self.out.set_color(&color_spec)?;
+       writeln!(self.out, ": {}.", diag.message)?;
 
         // Prints the source location if available.
         if let Some(source_map) = self.source_map() {
-            if diag.span().is_dummy() || diag.level == Level::Note {
-                return;
-            }
-            let info = source_map.lookup_pos_info(diag.span().start());
-            writeln!(self.out, "    File {file}:{line}:{col}",
-                file = info.file.name(),
-                line = info.line, col = info.col,
-            ).expect("error: failed to emit error");
+            let pos_info = source_map.lookup_pos_info(diag.span().start());
+            let line_number = pos_info.line.to_string();
+            let indent = line_number.len() + 2;
+
+            // Prints "  --> src/main.rs:1:1"
+            write!(self.out, "{:indent$}--> {file_name}:{line}:{col}",
+                "", indent = indent,
+                file_name = pos_info.file.name(),
+                line = pos_info.line,
+                col = pos_info.col,
+            )?;
         }
+        Ok(())
+    }
+}
+
+impl Emitter for TtyEmitter {
+    fn emit_diagnostic(&mut self, diag: &Diagnostic) {
+        self.try_emit_diagnostic(diag).expect("error: failed to emit error.");
     }
 
     fn source_map(&self) -> Option<&Rc<SourceMap>> {
